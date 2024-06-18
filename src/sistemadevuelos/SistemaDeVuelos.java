06d1e70405e64880dbd5decf96e20d074b7e0f1f
Brenda <salmasobrendaa@gmail.com>
2024-06-18 14:09:55 -0300

    Primer archivo

---
+++ src/sistemadevuelos/SistemaDeVuelos.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistemadevuelos;
+
+import sistemadevuelos.Formularios.Inicio;
+
+/**
+ *
+ * @author Brenda
+ */
+public class SistemaDeVuelos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // para el fondo
+        Inicio i = new Inicio();
+        
+        i.setVisible(true);
+        
+        i.setLocation(null);
+    }
+    
+}
